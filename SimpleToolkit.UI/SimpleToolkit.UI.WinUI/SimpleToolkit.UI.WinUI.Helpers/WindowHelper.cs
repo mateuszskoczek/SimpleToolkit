1272f6ad9fea7ca644a63f68fe381ca3f0aead54
Mateusz Skoczek <mateusz5432160@gmail.com>
2024-03-14 01:52:10 +0100

    new project added

---
+++ SimpleToolkit.UI/SimpleToolkit.UI.WinUI/SimpleToolkit.UI.WinUI.Helpers/WindowHelper.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleToolkit.UI.WinUI.Helpers
+{
+    public static partial class WindowHelper
+    {
+        #region PUBLIC METHODS
+
+        public static void ShowWindow(Window window)
+        {
+            var hwnd = WinRT.Interop.WindowNative.GetWindowHandle(window);
+
+            ShowWindow(hwnd, 0x00000009);
+
+            SetForegroundWindow(hwnd);
+        }
+
+        #endregion
+
+
+
+        #region PRIVATE METHODS
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        #endregion
+    }
+}
